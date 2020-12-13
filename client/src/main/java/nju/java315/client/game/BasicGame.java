b1cf36d6f624a685cc1c995e6f49e9a2a05435cd
zb-nju <181860106@smail.nju.edu.cn>
2020-12-14 00:15:39 +0800

    change balabala

--- client/src/main/java/nju/java315/client/game/BasicGame.java
+++ client/src/main/java/nju/java315/client/game/BasicGame.java
@@ -1,3 +1,9 @@
+/*
+ * @Author: zb-nju
+ * @Date: 2020-12-13 23:48:37
+ * @LastEditors: zb-nju
+ * @LastEditTime: 2020-12-14 00:15:24
+ */
 package nju.java315.client.game;
 
 import com.almasb.fxgl.app.GameApplication;
@@ -10,6 +16,7 @@ public class BasicGame extends GameApplication {
         settings.setWidth(800);
         settings.setHeight(600);
         settings.setTitle("Basic Game App");
+        System.out.println();
     }
 
 }
