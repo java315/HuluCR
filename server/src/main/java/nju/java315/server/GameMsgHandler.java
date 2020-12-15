260e8ca686caf35c8f01d877df166bd1d79bfd91
youngstudent2 <644699280@qq.com>
2020-12-15 11:48:05 +0800

    Create GameMsgHandler.java

---
+++ server/src/main/java/nju/java315/server/GameMsgHandler.java
@@ -0,0 +1,74 @@
+/*
+ * @Author: zb-nju
+ * @Date: 2020-12-13 23:41:23
+ * @LastEditors: zb-nju
+ * @LastEditTime: 2020-12-15 11:36:27
+ */
+package nju.java315.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.util.CharsetUtil;
+
+public class GameMsgHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    static private final Logger LOGGER = LoggerFactory.getLogger(GameMsgHandler.class);
+/*
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        LOGGER.info("receive client message : msgClzz={}, msgBody={}",msg.getClass(),msg);
+
+        BinaryWebSocketFrame frame = (BinaryWebSocketFrame)msg;
+        ByteBuf byteBuf = frame.content();
+
+        byte[] byteArray = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(byteArray);
+
+        for (byte b : byteArray) {
+            System.out.print(b);
+            System.out.print(", ");
+        }
+
+        System.out.println();
+
+    }*/
+    private HttpRequest request;
+ 
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            request = (HttpRequest) msg;
+            request.method();
+            String uri = request.uri();
+            System.out.println("Uri:" + uri);
+        }
+        if (msg instanceof HttpContent) {
+
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+
+            ByteBuf byteBuf = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
+            response.headers().add(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().add(HttpHeaderNames.CONTENT_LENGTH, byteBuf.readableBytes());
+
+            ctx.writeAndFlush(response);
+
+        }
+    }
+}
