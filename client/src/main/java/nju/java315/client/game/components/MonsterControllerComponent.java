24e3f332b83fca5fc3deb0ab0b3d776e983e8cf0
youngstudent2 <42881835+youngstudent2@users.noreply.github.com>
2021-01-21 12:06:21 +0800

    加入组件介绍

--- client/src/main/java/nju/java315/client/game/components/MonsterControllerComponent.java
+++
@@ -1,7 +0,0 @@
-package nju.java315.client.game.components;
-
-import com.almasb.fxgl.entity.component.Component;
-
-public class MonsterControllerComponent extends Component{
-    
-}
--- client/src/main/java/nju/java315/client/game/components/OwnerComponent.java
+++
@@ -1,9 +0,0 @@
-package nju.java315.client.game.components;
-
-import com.almasb.fxgl.entity.components.ObjectComponent;
-
-public class OwnerComponent extends ObjectComponent<Object> {
-    public OwnerComponent(Object entity) {
-        super(entity);
-    }
-}
